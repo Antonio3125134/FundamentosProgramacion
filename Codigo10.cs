eed9c9313eb92ec2804bdf93460b2c42cb17b448
Antonio3125134 <146687604+Antonio3125134@users.noreply.github.com>
2023-10-01 23:38:31 -0600

    Add files via upload

---
+++ Codigo10.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "Carlos Antonio", apellidos = "Bravo Hernández", completo;
+            completo = nombre + apellidos;
+            Console.WriteLine("sumar dos numeros");
+
+            int a = 50, b = 3, suma;
+            suma = a + b;
+            Console.WriteLine("* * bienvenidos * *" + completo + "* *");
+            Console.WriteLine("la operacion suma de A=" + a + "B=" + b);
+            Console.WriteLine($"tu resultado es {suma}");
+            Console.WriteLine("_GRACIAS");
+        }
+    }
+}
