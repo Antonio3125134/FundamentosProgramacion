d9340050e87285e438a3982989b7a51fc0aec58e
Antonio3125134 <146687604+Antonio3125134@users.noreply.github.com>
2023-10-31 19:50:03 -0600

    Add files via upload

---
+++ Unidad 3 While/Programa23U3.java
@@ -0,0 +1,15 @@
+package com.mycompany.programa23u3;
+
+public class Programa23U3 {
+
+    public static void main(String[] args) {
+        
+        int num=1;
+        do{
+            System.out.println(num);
+            num++;
+        }while(num<=10);
+        
+        
+    }
+}
