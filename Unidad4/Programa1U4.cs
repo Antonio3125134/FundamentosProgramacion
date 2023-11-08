b1c31aecd0c1f80c28fc829ee88905185f4b171c
Antonio3125134 <146687604+Antonio3125134@users.noreply.github.com>
2023-11-07 19:57:02 -0600

    Add files via upload

---
+++ Unidad4/Programa1U4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Programa1U4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] edades = new int[] { 17, 19, 18, 18, 17, };
+
+            int[] edades2 = { 17, 19, 18, 18, 18 };
+
+            for (int i = 0; i < edades.Length; i++)
+            {
+                Console.WriteLine("Indice: " + i + " es: " + edades[i]);
+                if (edades[i] >= 18)
+                    Console.WriteLine("Eres mayor!!");
+                else
+                    Console.WriteLine("Eres menor!!");
+            }
+        }
+    }
+}
---
+++ Unidad4/Programa2U4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Programa2U4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] calificaciones = new int[] { 100, 80, 95, 72, 34, 32, 78, 44, 67, 29 };
+            int suma, promedio;
+            suma= 0;
+            suma = suma + calificaciones[0];
+            promedio =suma;
+
+            for (int i = 0; i < calificaciones.Length; i++) {
+                Console.WriteLine("Calif: " + i + " es: " + calificaciones[i]);
+
+            }
+            Console.WriteLine("El promedio es: " + promedio);
+        }
+    }
+}
