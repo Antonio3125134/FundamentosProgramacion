c48588c0bb76a3c3236e3477e690f036eb02cfc8
Antonio3125134 <146687604+Antonio3125134@users.noreply.github.com>
2023-11-08 18:31:19 -0600

    Add files via upload

---
+++ Unidad4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Programa2U4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] calificaciones = new int[] { 100, 80, 95, 72, 34, 32, 78, 44, 67, 29 };
+            int suma = 0, promedio=0;
+            
+
+            for (int i = 0; i < calificaciones.Length; i++)
+            {
+                Console.WriteLine("Calif: " + i + " es: " + calificaciones[i]);
+                suma += calificaciones[i];
+
+                promedio = suma / calificaciones.Length;
+            }
+            Console.WriteLine("La suma de las calificaciones es: " + suma);
+            Console.WriteLine("El promedio es: " +promedio);
+        }
+    }
+}
