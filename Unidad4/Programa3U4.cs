7bcdb975ed2b9b71c606170f7f1d0d03f6e176a7
Antonio3125134 <146687604+Antonio3125134@users.noreply.github.com>
2023-11-09 19:55:59 -0600

    Add files via upload

---
+++ Unidad4/Programa3U4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Programa3U4
+{
+    internal class Programa3U4
+    {
+        static void Main(string[] args)
+        {
+            int tam;
+
+            Console.WriteLine("Escribe las temperaturas a procesar: ");
+            tam = Convert.ToInt32(Console.ReadLine());
+            int[] temp = new int[tam];
+            
+            for (int c = 0; c < temp.Length; c++)
+            {
+                Console.WriteLine("Escribe la temperatura "+c+" en Celcius: ");
+                temp[c] = Convert.ToInt32(Console.ReadLine());
+
+                double tempf = (temp[c] * 9 / 5) + 32;
+                double tempk = temp[c] + 273.15;
+
+                Console.WriteLine("Las temperaturas en Celcius son: " + temp[c] + "°");
+                Console.WriteLine("Las temperaturas en Farenheit son: " + tempf + "°");
+                Console.WriteLine("Las temperaturas en Kelvin son: " + tempk + "°");
+            }     
+        }
+    }
+}
